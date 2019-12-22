dcba8d6970da02c1860ea49a2495b7cb16525531
francnstein <franczale@icloud.com>
2019-12-21 20:26:13 -0500

    Weekly weather forecast  webscrapper

--- wscrapper.py
+++ wscrapper.py
@@ -1,4 +1,4 @@
-import bs4 as BeautifulSoup4
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,12 +10,24 @@ week = soup.find(id='seven-day-forecast-body')
 items = week.find_all(class_='tombstone-container')
 #print(items[0])
 
-print(items[0].find(class_='period-name').get_text())
-print(items[0].find(class_='short-desc').get_text())
-print(items[0].find(class_='temp').get_text())
+#print(items[0].find(class_='period-name').get_text())
+#print(items[0].find(class_='short-desc').get_text())
+#print(items[0].find(class_='temp').get_text())
 
 period_names = [item.find(class_='period-name').get_text()for item in items]
 short_descriptions = [item.find(class_='short-desc').get_text()for item in items]
-print(period_names)
-print(short_descriptions)
+temperatures = [item.find(class_='temp').get_text()for item in items]
 
+
+#print(period_names)
+#print(short_descriptions)
+#print(temperatures)
+
+weather_stuff = pd.DataFrame(
+    {
+     'period':period_names,
+     'short_descriptions':short_descriptions,
+     'temperatures': temperatures,
+    })
+
+print(weather_stuff)
